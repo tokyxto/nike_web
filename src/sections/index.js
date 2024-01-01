9540bf6c925f3895d5f872b945c9b0c0a9d5fd0c
tokyoto <76869932+tokyxto@users.noreply.github.com>
2024-01-01 21:32:06 +0200

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffers from "./SpecialOffers";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+    Services,
+    Subscribe,
+    SpecialOffers
+}
